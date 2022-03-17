c7c62dcbc8283f2574fcf37f92a54e49616f1997
Kim-kwan-woo <kw2577@naver.com>
2022-03-17 21:25:57 +0900

    make view

---
+++ presentation/src/main/java/com/kwanwoo/android/presentation/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kwanwoo.android.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kwanwoo.android.presentation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
