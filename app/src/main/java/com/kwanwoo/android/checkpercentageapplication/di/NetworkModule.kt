85fa67cf265e84250e59cfbef20797af2760d661
Kim-kwan-woo <kw2577@naver.com>
2022-03-26 20:02:19 +0900

    set dagger hilt

---
+++ app/src/main/java/com/kwanwoo/android/checkpercentageapplication/di/NetworkModule.kt
@@ -0,0 +1,62 @@
+package com.kwanwoo.android.checkpercentageapplication.di
+
+//import com.pss.check_percentage.utils.Utils.BASE_URL
+//import com.pss.data.remote.api.LoveCalculatorApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(10, TimeUnit.SECONDS)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(getLoggingInterceptor())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("BASE_URL")
+            .client(okHttpClient)
+            .client(provideHttpClient())
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+//    @Provides
+//    @Singleton
+//    fun provideLoveCalculatorApiService(retrofit: Retrofit): LoveCalculatorApi {
+//        return retrofit.create(LoveCalculatorApi::class.java)
+//    }
+
+
+    private fun getLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+
+
+}
--- presentation/src/main/java/com/kwanwoo/android/presentation/view/MainActivity.kt
+++ presentation/src/main/java/com/kwanwoo/android/presentation/view/MainActivity.kt
@@ -2,12 +2,16 @@ package com.kwanwoo.android.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.kwanwoo.android.presentation.R
 import com.kwanwoo.android.presentation.base.BaseActivity
 import com.kwanwoo.android.presentation.databinding.ActivityMainBinding
+import com.kwanwoo.android.presentation.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-
+    private val mainViewModel by viewModels<MainViewModel>()
     override fun init() {
 
     }
