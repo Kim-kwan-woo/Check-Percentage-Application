40363361a9e8a17007ee21a90e20772e3d8aae51
Kim-kwan-woo <kw2577@naver.com>
2022-03-29 20:42:55 +0900

    set appbar

--- presentation/src/main/java/com/kwanwoo/android/presentation/view/MainActivity.kt
+++ presentation/src/main/java/com/kwanwoo/android/presentation/view/MainActivity.kt
@@ -7,12 +7,13 @@ import com.kwanwoo.android.presentation.R
 import com.kwanwoo.android.presentation.base.BaseActivity
 import com.kwanwoo.android.presentation.databinding.ActivityMainBinding
 import com.kwanwoo.android.presentation.viewmodel.MainViewModel
+import com.pss.barlibrary.CustomBar.Companion.setTransparentBar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val mainViewModel by viewModels<MainViewModel>()
     override fun init() {
-
+        setTransparentBar(this)
     }
 }
